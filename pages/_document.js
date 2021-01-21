58c4bf0d5d3a12df61312d93aa5d588254e8d1e8
AnimaFPS <lukecsnz@gmail.com>
2021-01-22 00:11:18 +1300

    Optimized font loading

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+          <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
           <meta charset="utf-8"/>
         </Head>
         <body>
