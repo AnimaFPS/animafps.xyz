ec4ac648287488b008de2c193e92f270a9e78a9c
AnimaFPS <lukecsnz@gmail.com>
2021-01-25 12:11:19 +1300

    Security config

--- next.config.js
+++ next.config.js
@@ -12,6 +12,14 @@ module.exports = {
                     {
                         key: 'X-Content-Type-Options',
                         value: 'nosniff'
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block'
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: 'default-src https:'
                     }
                 ]
             }
